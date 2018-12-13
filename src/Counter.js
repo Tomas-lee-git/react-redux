fe8020f850e87f10502d627a13530292a150ebd8
mulili <muli9611@gmail.com>
2018-12-13 19:11:18 +0800

    Transforming create-react-app

---
+++ src/Counter.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './Counter.less';
+class Counter extends React.Component{
+    constructor(props){
+        super(props);
+        
+    }
+
+
+
+    render(){
+        return(
+            <div>hello</div>
+        )
+    }
+}
+
+export default Counter;  
